25428e791b8b66b35b404429023c0ada42ad0152
Rajan Anand <ra9303@rit.edu>
2022-04-20 22:57:33 -0400

    SATD identified number 5

--- org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/graph/algo/DisjointSetsAlgo.java
+++ org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/graph/algo/DisjointSetsAlgo.java
@@ -54,6 +54,7 @@ public class DisjointSetsAlgo {
             for (V v : getNextVertices.apply(u)) {
                 List<V> uset = forest.get(u);
                 List<V> vset = forest.get(v);
+//                TODO: Use equals to compare object references
                 if (uset != vset) {
                     MinimumSpanningTreeAlgo.union(uset, vset, forest);
                 }
